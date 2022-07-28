55d9c38434e9401106c052195cafadc4d732d677
CrumblingAway <antonkid@gmail.com>
2022-07-28 22:55:30 +0300

    main: Extract misc data from last two lines.

--- main.py
+++ main.py
@@ -5,15 +5,35 @@ FIRST_NAME_KEY = 0x86B7
 SECOND_NAME_KEY = 0x9E60
 PHONE_KEY = 0x5159
 TIME_KEY = 0xD812
+MISC_KEY = 0x6704
+
+class Contact:
+
+    def __init__(self, id, first_name, second_name, phone, time, misc):
+        self.id = id
+        self.first_name = first_name
+        self.second_name = second_name
+        self.phone = phone
+        self.time = time
+        self.misc = misc
+
+    def __repr__(self):
+        return f"id: {self.id}, first_name: {self.first_name}, second_name: {self.second_name}, phone: {self.phone}," \
+               f"time: {self.time}"
 
 
 def read_encoded_line(line):
+    """
+    Parses line into (id, value) pairs
+    :param line:
+    :return:
+    """
     results = dict()
     start_idx = 0
 
     while start_idx < len(line):
         id = line[start_idx: start_idx + ID_LENGTH]
-        name_length = int(line[start_idx + ID_LENGTH: start_idx + ID_LENGTH + NUM_OF_DIGITS_LENGTH], 16) # TODO: CHECK IF HEX
+        name_length = int(line[start_idx + ID_LENGTH: start_idx + ID_LENGTH + NUM_OF_DIGITS_LENGTH], 16)
         name = line[start_idx + ID_LENGTH + NUM_OF_DIGITS_LENGTH: start_idx + ID_LENGTH + NUM_OF_DIGITS_LENGTH + name_length]
         results[id] = name
 
@@ -24,25 +44,18 @@ def read_encoded_line(line):
 if __name__ == '__main__':
     encoded_contact_info_file = open("ex_v8.txt", "r")
 
-    first_name_results = dict()
-    second_name_results = dict()
-    phone_results = dict()
-    time_results = dict()
+    decoded_results = {
+        FIRST_NAME_KEY: dict(),
+        SECOND_NAME_KEY: dict(),
+        PHONE_KEY: dict(),
+        TIME_KEY: dict(),
+        MISC_KEY: dict()
+    }
 
     while current_line := encoded_contact_info_file.readline().rstrip():
         line_key = int(current_line[:4], 16)
         current_line = current_line[4:]
 
-        if line_key == FIRST_NAME_KEY:
-            first_name_results = read_encoded_line(current_line)
-        elif line_key == SECOND_NAME_KEY:
-            second_name_results = read_encoded_line(current_line)
-        elif line_key == PHONE_KEY:
-            phone_results = read_encoded_line(current_line)
-        elif line_key == TIME_KEY:
-            time_results = read_encoded_line(current_line)
+        decoded_results[line_key] = read_encoded_line(current_line)
 
     encoded_contact_info_file.close()
-
-    for id in first_name_results:
-        print(f"{id}: {first_name_results[id]} {second_name_results[id]} {phone_results[id]} {time_results[id]}")
