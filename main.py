eb2e26bab6ff62e538370ee612f4e1e2e6dc99dd
CrumblingAway <antonkid@gmail.com>
2022-07-29 00:54:57 +0300

    main: Save results in decoded_info.csv .

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 ID_LENGTH = 4
-NUM_OF_DIGITS_LENGTH = 5
+LENGTH_OF_VALUE_LENGTH = 5
 
 FIRST_NAME_KEY = 0x86B7
-SECOND_NAME_KEY = 0x9E60
+LAST_NAME_KEY = 0x9E60
 PHONE_KEY = 0x5159
 TIME_KEY = 0xD812
 MISC_KEY = 0x6704
+KEYS = [FIRST_NAME_KEY, LAST_NAME_KEY, PHONE_KEY, TIME_KEY, MISC_KEY]
 
 class Contact:
 
@@ -29,15 +30,20 @@ def read_encoded_line(line):
     :return:
     """
     results = dict()
-    start_idx = 0
+    start = 0
 
-    while start_idx < len(line):
-        id = line[start_idx: start_idx + ID_LENGTH]
-        name_length = int(line[start_idx + ID_LENGTH: start_idx + ID_LENGTH + NUM_OF_DIGITS_LENGTH], 16)
-        name = line[start_idx + ID_LENGTH + NUM_OF_DIGITS_LENGTH: start_idx + ID_LENGTH + NUM_OF_DIGITS_LENGTH + name_length]
-        results[id] = name
+    while start < len(line):
+        # Read id
+        id_end = start + ID_LENGTH
+        id = line[start: id_end]
 
-        start_idx += ID_LENGTH + NUM_OF_DIGITS_LENGTH + name_length
+        # Read value
+        value_length = int(line[id_end: id_end + LENGTH_OF_VALUE_LENGTH], 16)
+        value_start = start + ID_LENGTH + LENGTH_OF_VALUE_LENGTH
+        value = line[value_start: value_start + value_length]
+        results[id] = value
+
+        start += ID_LENGTH + LENGTH_OF_VALUE_LENGTH + value_length
 
     return results
 
@@ -46,16 +52,30 @@ if __name__ == '__main__':
 
     decoded_results = {
         FIRST_NAME_KEY: dict(),
-        SECOND_NAME_KEY: dict(),
+        LAST_NAME_KEY: dict(),
         PHONE_KEY: dict(),
         TIME_KEY: dict(),
         MISC_KEY: dict()
     }
+    id_set = set()
 
     while current_line := encoded_contact_info_file.readline().rstrip():
         line_key = int(current_line[:4], 16)
         current_line = current_line[4:]
+        decoded_line = read_encoded_line(current_line)
 
-        decoded_results[line_key] = read_encoded_line(current_line)
+        decoded_results[line_key].update(decoded_line)
+        for id in decoded_line:
+            id_set.add(id)
 
     encoded_contact_info_file.close()
+
+    decoded_contact_info_file = open("decoded_info.csv", "w")
+    decoded_contact_info_file.write("id,first_name,last_name,phone,time,misc\n")
+    for id in id_set:
+        line = f"{id}"
+        for key in KEYS:
+            value = decoded_results[key].get(id)
+            line += f",{value if value is not None else 'None'}"
+        decoded_contact_info_file.write(line + '\n')
+    decoded_contact_info_file.close()
